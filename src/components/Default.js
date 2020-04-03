e9b46144c904d20b139b4c94bc234f376c3681f2
Miriam <miriamhall.mgh@gmail.com>
2020-04-02 20:13:58 -0700

    Cart fully functional

--- src/components/Default.js
+++ src/components/Default.js
@@ -3,10 +3,18 @@ import React, {Component} from 'react';
 class Default extends Component {
     render() {
         return (
-            <div>
-                <h3>Page Not Found.</h3>
+            <div className="container">
+                <div className="row">
+                    <div className="col-10 mx-auto text-center text-title text-uppercase py-5">
+                        <h1 className="display-3">404</h1>
+                        <h1>error</h1>
+                        <h2>page not found</h2>
+                        <h3>the requested URL <span className="text-danger"><strong>{this.props.location.pathname}</strong></span> was not found.</h3>
+                    </div>
+                </div>
             </div>
-        );
+        )
+        ;
     }
 }
 
--- src/context.js
+++ src/context.js
@@ -76,15 +76,59 @@ class ProductProvider extends Component {
     }
 
     increment = (id) => {
-        console.log('increment method');
-    }
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item=>item.id===id);
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+        product.count = product.count + 1;
+        product.total = product.price * product.count;
+        this.setState(
+            ()=>{
+                return{ cart:[...tempCart]
+            }},
+            ()=>{
+                this.addTotals();
+            })
+    };
 
     decrement = (id) => {
-       console.log('decrement method');
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item=>item.id===id);
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+        product.count -= 1;
+        if (product.count === 0) {
+            this.removeItem(id);
+        } else {
+            product.total = product.price * product.count;
+            this.setState(
+            ()=>{
+                return{ cart:[...tempCart]
+            }},
+            ()=>{
+                this.addTotals();
+            })
+        };
     };
 
     removeItem = (id) => {
-       console.log('item removed - method in context');
+       let tempProducts = [...this.state.products];
+       let tempCart = [...this.state.cart];
+       tempCart = tempCart.filter(item => item.id !== id);
+       const index = tempProducts.indexOf(this.getItem(id));
+       let removedProduct = tempProducts[index];
+       removedProduct.inCart = false;
+       removedProduct.count = 0;
+       removedProduct.total = 0;
+       this.setState(
+           ()=>{
+               return {
+                   cart: [...tempCart],
+                   products: [...tempProducts]
+           }},
+           ()=>{
+               this.addTotals();
+           })
     };
 
     clearCart = () => {
